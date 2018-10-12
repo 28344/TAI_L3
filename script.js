dd35c7c002f98cd912c88fbf1c4adae77c94d3dd
student <28344@student.pwsztar.edu.pl>
2018-10-12 13:59:02 +0200

    quiz

---
+++ script.js
@@ -0,0 +1,75 @@
+let questions =
+    [
+        {
+        number: 1,
+        question:'1. Jakiego typu grą jest Call of Duty?',
+        answers: ['FPS','MMOPRG','Zręcznościowa','Logiczna'],
+        correct_answer:'FPS'
+        },
+        {
+         number: 2,
+         question:'2. Jakiego koloru jest niebieski maluch?',
+         answer: ['Niebieskiego', 'Czerwonego','Żółtego','Czarnego'],
+         correct_answer:'Niebieskiego'
+        },
+        {
+        number: 3,
+        question:'3. Jaka jest prędkość światła?',
+        answer:['Nie wiem', '123 456 7 kmh','300 000 kmsek','90kmh'],
+        correct_answer:'300 000 kmsek'
+        },
+
+        {
+        number:4,
+        question:'4. Ziemia jest?',
+        answer:['Kulą','Płaska','Elipsoidą','Prostokątem'],
+        correct_answer:'Elipsoidą'
+        },
+        {
+        number: 5,
+        question:'5. Modelem mercedessa jest?',
+        answer:['W123','E12','A5','WD40'],
+        correct_answer:'W123'
+        },
+        {
+        number:6,
+        question:'6. Top Gun to film...?',
+        answer:['Sensacyjny','Dramat','Komedia','Wojenny'],
+        correct_answer:'Sensacyjny'
+        },
+        {
+        number:7,
+        question:'7. Stolica Australii jest...?',
+        answer:['Melbourne','Canberra','Sydney','Delhi'],
+        correct_answer:'Canberra'
+        },
+        {
+        number:8,
+        question:'8. Więcej niż jedno zwierze to?',
+        answer:['Lama','Okoń','Linijka','Wataha'],
+        correct_answer:'Wataha'
+        },
+        {
+        number:9,
+        question:'9. Co pływa po kanale weneckim?',
+        answer:['Gondola','Ponton','Galera','Żaglówka'],
+        correct_answer:'Gondola'
+        },
+        {
+        number:10,
+        question:'10. Jak nazywa się tradycyjny napój Anglików pity po południu?',
+        answer:['Kawa','Mleko','Herbata','Sok'],
+        correct_answer:'Herbata'
+        }
+    ];
+
+
+let button=document.querySelector('.next');
+button.addEventListener('click',function (evt) {
+
+})
+
+
+
+
+
